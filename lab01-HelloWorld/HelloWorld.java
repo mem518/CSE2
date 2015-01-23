e62c3b2a5ac1ee97ac4bd19d122d7f69d1e62dce
mem518 <mem518@lehigh.edu>
2015-01-23 15:59:08 +0000

    update HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+// Manuel E. Mendez
+// Demo 0
+// Hello World Java Program
+//
+// first complile the program 
+//      javac HelloWorld.java
+//
+// run the program
+//      java HelloWolrd//
+
+//Define a class
+public class HelloWorld{
+    
+    // add mani method
+    public static void main(String [] args) {
+        //Print the statment with the box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
