92748c9495560c5faa5f65ada2ffbf3b00fba118
mem518 <mem518@lehigh.edu>
2015-01-27 23:10:12 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+// Manuel E. Mendez
+//1/27/14
+//CSE 2
+//Welcome Class Java Program
+
+//Define a class
+public class WelcomeClass{
+    
+    // add mani method
+    public static void main(String [] args) {
+        //Print the welcome message with the Lehigh ID and the tweet.
+         System.out.println("  -----------");
+         System.out.println("  | WELCOME |");
+         System.out.println("  -----------");
+         System.out.println("  ^  ^  ^  ^  ^  ^");
+         System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+         System.out.println("<-M--E--M--5--1--8->");
+         System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+         System.out.println("  v  v  v  v  v  v  ");
+         System.out.println("I have been playing the alto saxophone since the sixth grade, but I stopped playing around 10th grade. I will hopefully be getting back into playing the instrument by the end of Februrary.");
+         
+         //test/testtest
+    }
+    
+}
